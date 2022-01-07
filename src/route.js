454536ca2945a11e01c673ee4e6bddb9ae9239f2
Arman Sargsyan <arman.sargsyan.fdgmail.com>
2022-01-07 12:02:55 +0400

    Add Router for books

---
+++ src/route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getAllBooks, getBooksById, addBooks, updateBooks, deleteBooks } from "./controllers.js";
+
+
+export const router = express.Router();
+
+router.route("/").get(getAllBooks).post(addBooks);
+router.route("/:id").get(getBooksById).put(updateBooks).delete(deleteBooks);
